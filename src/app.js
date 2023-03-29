1f94752ec57c9a817b344c774c436934699a98dd
Fernando castro <fernandocastro77@yahoo.com>
2023-03-29 00:03:54 -0400

    just rended three divs

---
+++ src/app.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+const Pet = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Luna'),
+        React.createElement('h2', {}, 'Dog'),
+        React.createElement('h2', {}, 'Doberman'),
+    ]);
+};
+
+const App = () => {
+    return React.createElement('div', {}, [
+        React.createElement('h1', {}, 'Adopt Me!'),
+        React.createElement(Pet),
+        React.createElement(Pet),
+        React.createElement(Pet),
+
+    ]);
+};
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
